3f8f60e3c8de4f155b9212e5498c0aadd283101c
Morten Madsen <morten.madsen.92@gmail.com>
2018-02-12 20:50:45 +0100

    Added username and password textboxes on account information page.

--- TimeSync.UI/UserAccount.xaml.cs
+++ TimeSync.UI/UserAccount.xaml.cs
@@ -24,5 +24,23 @@ namespace TimeSync.UI
         {
             InitializeComponent();
         }
+
+        private void ButtonBase_OnClickUserAccount(object sender, RoutedEventArgs e)
+        {
+            UserAccount userAccount = new UserAccount();
+            this.NavigationService.Navigate(userAccount);
+        }
+
+        private void ButtonBase_OnClickToolkits(object sender, RoutedEventArgs e)
+        {
+            Toolkits toolkits = new Toolkits();
+            this.NavigationService.Navigate(toolkits);
+        }
+
+        private void ButtonBase_OnClickTimeregistrations(object sender, RoutedEventArgs e)
+        {
+            Timeregistrations timeregistrations = new Timeregistrations();
+            this.NavigationService.Navigate(timeregistrations);
+        }
     }
 }
