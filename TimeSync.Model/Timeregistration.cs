841cea54cbf980e509fc6c93f80acd5ada8e50a2
Morten Madsen <morten.madsen.92@gmail.com>
2018-03-11 17:46:22 +0100

    Finished implementation of duration parsing. Added content to ToolkitsPage and TimeregistrationsPage. Added 'wrapper' class acting as temporary data container to be put into toolkitInfo object when saving data.

--- TimeSync.Model/Timeregistration.cs
+++ TimeSync.Model/Timeregistration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -20,7 +21,7 @@ namespace TimeSync.Model
         public bool AddHours(string duration)
         {
             double hours;
-            bool durationIsNumberOfHours = double.TryParse(duration, out hours);
+            var durationIsNumberOfHours = double.TryParse(duration, out hours);
             if (durationIsNumberOfHours)
             {
                 Hours = hours;
@@ -28,8 +29,8 @@ namespace TimeSync.Model
             }
             else
             {
-                // expected string format "08(:,.)00 - 08(:,.)30"
-                bool stringFormattedAsExpected = ParseDurationString(duration, out hours);
+                var stringFormattedAsExpected = ParseDurationString(duration, out hours);
+                Hours = hours;
                 return stringFormattedAsExpected;
             }
             
@@ -38,6 +39,7 @@ namespace TimeSync.Model
         private bool ParseDurationString(string duration, out double hours)
         {
             hours = 0;
+            // expected string format "08(:,.)00 - 08(:,.)30"
             var result = Regex.Matches(duration, @"([\d]+)[\:\,\.]([\d]+)|([\d]+)");
             if (result.Count == 0)
             {
@@ -53,16 +55,6 @@ namespace TimeSync.Model
                 var timespan = to.Subtract(from);
                 hours = timespan.TotalHours;
 
-                //if (ContainsTimeDelimiter(fromTime) && ContainsTimeDelimiter(toTime))
-                //{
-                //    hours = CalculateTimeDurationWithDelimiter(fromTime, toTime);
-                //}
-                //else
-                //{
-                //    hours = CalculateTimeDurationWithoutDelimiter(fromTime, toTime);
-                //}
-
-
                 return true;
             }
         }
@@ -72,46 +64,21 @@ namespace TimeSync.Model
             DateTime dt;
             if (time.Contains(":"))
             {
-                bool couldParse = DateTime.TryParse(time, out dt);
+                var couldParse = DateTime.TryParse(time, out dt);
             }
             else if (time.Contains(",") || time.Contains("."))
             {
                 var idx = time.IndexOfAny(",.".ToCharArray());
-                var newTime = $"{time.Substring(0, idx)}{time.Substring(idx)}";
-                bool couldParse = DateTime.TryParse(newTime, out dt);
+                var newTime = $"{time.Substring(0, idx)}:{time.Substring(idx)}";
+                var couldParse = DateTime.TryParse(newTime, out dt);
             }
             else
             {
                 var newTime = $"{time.Substring(0, 2)}:{time.Substring(3)}";
-                bool couldParse = DateTime.TryParse(newTime, out dt);
+                var couldParse = DateTime.TryParse(newTime, out dt);
             }
 
             return dt;
-        }
-
-        private double CalculateTimeDurationWithoutDelimiter(string fromTime, string toTime)
-        {
-            return 0;
-        }
-
-        private double CalculateTimeDurationWithDelimiter(string fromTime, string toTime)
-        {
-            DateTime fromDateTime;
-            DateTime toDateTime;
-
-            bool couldParseFrom = DateTime.TryParse(fromTime, out fromDateTime);
-            bool couldParseTo = DateTime.TryParse(toTime, out toDateTime);
-
-            if (couldParseFrom && couldParseTo)
-            {
-                
-            }
-            return 0;
-        }
-
-        private bool ContainsTimeDelimiter(string time)
-        {
-            return time.Any(element => ":".ToCharArray().Contains((element)));
         }
     }
 }
---
+++ TimeSync.Tests/TimeregistrationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeSync.DataAccess;
+using TimeSync.Model;
+
+namespace TimeSync.Tests
+{
+    [TestClass]
+    public class TimeregistrationTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CalculateDuration()
+        {
+            //Arrange
+            string duration1 = "08:00-08:30";
+            string duration2 = "08.00-09.00";
+            string duration3 = "08,00-09,30";
+            string duration4 = "0800-1000";
+            var timereg1 = new Timeregistration();
+            var timereg2 = new Timeregistration();
+            var timereg3 = new Timeregistration();
+            var timereg4 = new Timeregistration();
+
+            //Act
+            var result1 = timereg1.AddHours(duration1);
+            var result2 = timereg2.AddHours(duration2);
+            var result3 = timereg3.AddHours(duration3);
+            var result4 = timereg4.AddHours(duration4);
+
+            //Assert
+            Assert.IsTrue(result1);
+            Assert.AreEqual(timereg1.Hours, 0.5);
+
+            Assert.IsTrue(result2);
+            Assert.AreEqual(timereg2.Hours, 1.0);
+
+            Assert.IsTrue(result3);
+            Assert.AreEqual(timereg3.Hours, 1.5);
+
+            Assert.IsTrue(result4);
+            Assert.AreEqual(timereg4.Hours, 2);
+        }
+    }
+}
