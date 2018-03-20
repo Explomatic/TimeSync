0aae6ab9d603c4facaa938035a335d5ce723b35f
Morten Madsen <morten.madsen.92@gmail.com>
2018-03-20 20:46:25 +0100

    Model changes 2

---
+++ TimeSync.Model/Toolkit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.SharePoint.Client;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSync.Model
+{
+    public class Toolkit 
+    {
+        public int UserId { get; set; }
+        public string Url { get; set; }
+        public bool HasTimeSlots { get; set; }
+        public string CustomerName { get; set; }
+        public List<TimeSlot> TimeSlots { get; set; }
+    }
+    //ReSharper disable InconsistentNaming
+    public class TimeSlot
+    {
+        public bool IsFieldLookup { get; set; }
+        public string FieldName { get; set; }
+        public List<TimeInterval> TimeIntervals { get; set; }
+
+        public void GetSPQuery(ListItem listItem, TimeInterval timeInterval)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct TimeInterval
+    {
+        public string Id;
+        public string Interval;
+    }
+}
---
+++ TimeSync.UI/ViewModel/ToolkitViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TimeSync.DataAccess;
+using TimeSync.Model;
+
+namespace TimeSync.UI.ViewModel
+{
+    class ToolkitViewModel : BaseViewModel
+    {
+        private readonly IRepository<List<Toolkit>> _repo;
+        private List<Toolkit> _toolkits;
+
+        public ObservableCollection<Toolkit> ListOfToolkits { get; set; }
+
+        public TimeManager TimeManager { get; set; }
+
+        public ToolkitViewModel()
+        {
+            _repo = new Repository<List<Toolkit>>("ToolkitSaveLocation");
+            ListOfToolkits = new ObservableCollection<Toolkit>();
+            _toolkits = _repo.GetData();
+            //PopulateWrapperClass(_toolkitInfo);
+        }
+
+        //private void PopulateWrapperClass(ToolkitInfo toolkitInfo)
+        //{
+        //    foreach (KeyValuePair<string, Toolkit> entry in toolkitInfo.Toolkits)
+        //    {
+        //        ListOfToolkits.Add(new ToolkitInfoViewModelWrapper { ToolkitName = entry.Key, ToolkitUrl = entry.Value.Url });
+        //    }
+        //}
+
+        public ICommand AddNewToolkitCommand => new DelegateCommand(AddNewToolkit);
+
+        public void AddNewToolkit()
+        {
+            ListOfToolkits.Add(new Toolkit());
+        }
+
+        public ICommand SaveToolkitsCommand => new DelegateCommand(SaveToolkits);
+
+        public void SaveToolkits()
+        {
+            //PopulateToolkitInfoObject(ListOfToolkits);
+            TimeManager.SaveToolkitInfo(TimeManager.UserInfo, _toolkits);
+        }
+
+    //    private void PopulateToolkitInfoObject(ObservableCollection<ToolkitInfoViewModelWrapper> listOfToolkits)
+    //    {
+    //        foreach (var toolkitInfoWrapper in listOfToolkits)
+    //        {
+    //            Toolkit toolkit = new Toolkit()
+    //            {
+    //                Url = toolkitInfoWrapper.ToolkitUrl
+    //            };
+    //            if ( !_toolkitInfo.Toolkits.ContainsKey(toolkitInfoWrapper.ToolkitName) )
+    //            {
+    //                _toolkitInfo.Toolkits.Add(toolkitInfoWrapper.ToolkitName, toolkit);
+    //            }
+    //        }     
+    //    }
+    }
+
+    class ToolkitInfoViewModelWrapper
+    {
+        public string ToolkitName { get; set; }
+        public string ToolkitUrl { get; set; }
+    }
+}
